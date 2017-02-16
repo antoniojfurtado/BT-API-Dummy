2e27495a472c837dba021b833aac018d3258018e
Antonio Furtado <antonio@ajency.in>
2017-02-16 19:02:04 +0530

    Modofed Jsons to reflect business cases

--- bt-asn-api.php
+++ bt-asn-api.php
@@ -7,35 +7,26 @@ error_reporting(E_ALL);
 $type = isset($_GET['type']) ? $_GET['type'] : 'res';
 $api = $_GET['api'];
 
-$apis = [
-'createASN' => 'POST',
-'addProducttoExistingASN' => 'POST',
+$dir    = './res';
+$files1 = scandir($dir);
+$apis = array_diff($files1, array('.', '..'));
 
-'listASNs' => 'GET',
-'getASNLineItems' => 'GET',
-
-'updateASN' => 'POST',
-'addBoxestoASN' => 'POST',
-
-
-];
-
-if(!in_array($_GET['api'],array_keys($apis))) {
-    return print 'Pass api parameter as '.implode(' | ',array_keys($apis));
+if(!in_array($_GET['api'],$apis)) {
+    return print 'Pass api parameter as '.implode(' | ',$apis);
 }
 
-$file_content = file_get_contents('./'.$type.'/'.$api.'.json');
+$file_content = file_get_contents('./'.$type.'/'.$api);
 
 function is_json($string,$return_data = false) {
     $data = json_decode($string);
     return (json_last_error() == JSON_ERROR_NONE) ? ($return_data ? $data : TRUE) : json_encode(["Invalid Json"]);
 }
 
-if($apis[$_GET['api']] == 'POST' || $type == 'res'){
+if (0 === strpos($file_content, '?')) {
+    print $file_content;
+} else {
     header('Content-Type: application/json');
     if(is_json($file_content)){
         print $file_content;
     }
-} else {
-    print $file_content;
 }
